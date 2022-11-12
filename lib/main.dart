16c97037476272e8cfab3a0f1e41c5a35671c09f
ClementPeter <61213263+ClementPeter@users.noreply.github.com>
2022-11-13 00:35:44 +0100

    Flutter tawk virtual support

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tawk_app/screens/support_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -48,13 +49,12 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton.extended(
           onPressed: () {
-                  //  Navigator.push(
-                  //   context,
-                  //   MaterialPageRoute(
-                  //     builder: (context) =>
-                  //       // SupportPage(),
-                  //   ),
-                  // );
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => SupportPage(),
+              ),
+            );
           },
           tooltip: 'Increment',
           label: Row(
--- lib/screens/support_page.dart
+++ lib/screens/support_page.dart
@@ -1,6 +1,5 @@
 // // import 'package:flutter/material.dart';
 
-
 // // class SupportPage extends StatelessWidget {
 // //   const SupportPage({super.key});
 
@@ -10,41 +9,45 @@
 // //   }
 // // }
 
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// import 'package:flutter_tawk/flutter_tawk.dart';
+import 'package:flutter_tawk/flutter_tawk.dart';
 
-// void main() => runApp(const MyApp());
+class SupportPage extends StatelessWidget {
+  const SupportPage({Key? key}) : super(key: key);
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: const Text('Flutter Tawk'),
-//           backgroundColor: const Color(0XFFF7931E),
-//           elevation: 0,
-//         ),
-//         body: Tawk(
-//           directChatLink: 'YOUR_DIRECT_CHAT_LINK',
-//           visitor: TawkVisitor(
-//             name: 'Ayoub AMINE',
-//             email: 'ayoubamine2a@gmail.com',
-//           ),
-//           onLoad: () {
-//             print('Hello Tawk!');
-//           },
-//           onLinkTap: (String url) {
-//             print(url);
-//           },
-//           placeholder: const Center(
-//             child: Text('Loading...'),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tawk Support'),
+        //backgroundColor: const Color(0XFFF7931E),
+        elevation: 0,
+      ),
+      body: Tawk(
+        directChatLink:
+            'https://tawk.to/chat/63700c4db0d6371309ceb555/1ghmr040j',
+        visitor: TawkVisitor(
+          name: 'Visitor 007',
+          email: '007@gmail.com',
+        ),
+        onLoad: () {
+          print('Hello Tawk!');
+        },
+        onLinkTap: (String url) {   
+          print(url);
+        },
+        placeholder: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              CircularProgressIndicator(),
+              SizedBox(height: 10),
+              Text("Loading...")
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
