b8ba5bda4e89d8675f8ec18d6b87aeea126e2028
ClementPeter <61213263+ClementPeter@users.noreply.github.com>
2022-11-12 22:54:16 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Virtual Assistant',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Virtual Assistant'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text(
+              'Flutter Real time Virtual Chat assistant using Flutter tawk',
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+                  //  Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(
+                  //     builder: (context) =>
+                  //       // SupportPage(),
+                  //   ),
+                  // );
+          },
+          tooltip: 'Increment',
+          label: Row(
+            children: [
+              Icon(Icons.support_agent),
+              SizedBox(width: 5.0),
+              Text("Live Assistant"),
+            ],
+          )),
+    );
+  }
+}
---
+++ lib/screens/support_page.dart
@@ -0,0 +1,50 @@
+// // import 'package:flutter/material.dart';
+
+
+// // class SupportPage extends StatelessWidget {
+// //   const SupportPage({super.key});
+
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return Container();
+// //   }
+// // }
+
+// import 'package:flutter/material.dart';
+
+// import 'package:flutter_tawk/flutter_tawk.dart';
+
+// void main() => runApp(const MyApp());
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Flutter Tawk'),
+//           backgroundColor: const Color(0XFFF7931E),
+//           elevation: 0,
+//         ),
+//         body: Tawk(
+//           directChatLink: 'YOUR_DIRECT_CHAT_LINK',
+//           visitor: TawkVisitor(
+//             name: 'Ayoub AMINE',
+//             email: 'ayoubamine2a@gmail.com',
+//           ),
+//           onLoad: () {
+//             print('Hello Tawk!');
+//           },
+//           onLinkTap: (String url) {
+//             print(url);
+//           },
+//           placeholder: const Center(
+//             child: Text('Loading...'),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
